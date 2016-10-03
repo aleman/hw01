2d36e6d75070c8971e1dbe5d5d6f7f2b12789a8a
B. Aleman Meza <balemanm@nsm.uh.edu>
2016-10-03 14:59:35 -0500

    Simplified example, it fails with one test case

--- build.sh
+++ build.sh
@@ -4,6 +4,12 @@ echo "Environment: `uname -a`"
 echo "Compiler: `$CXX --version`"
 
 $CXX -std=c++11 *cpp -o notify || exit 1
-./notify "input=test-a.1.txt" | diff - test-a.out.txt || exit 1
-./notify "input=test-b.1.txt" | diff - test-b.out.txt || exit 1
-./notify "input=test-z.1.txt" | diff - test-z.out.txt || exit 1
+echo ""
+echo "test a"
+./notify "test-a.1.txt" | diff - test-a.out.txt || exit 1
+echo ""
+echo "test b"
+./notify "test-b.1.txt" | diff - test-b.out.txt || exit 1
+echo ""
+echo "test z"
+./notify "test-z.1.txt" | diff - test-z.out.txt || exit 1
--- notify.cpp
+++ notify.cpp
@@ -74,7 +74,7 @@ int main( int argc, char** argv )
 	a.readFileContents();
 
 	for( int i = 0; i < a.getNumberOfLines(); i++ ) {
-		cout << a.lineAt(i) << endl;
+		cout << i << "\t" << a.lineAt(i) << endl;
 	}
 }
 
