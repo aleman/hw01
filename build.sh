5c6d9f9db65d4920c76fcbc1b91a7b385c2a1d45
B. Aleman Meza <balemanm@nsm.uh.edu>
2016-10-03 14:45:11 -0500

    Added two more test cases into build.sh

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ echo "Compiler: `$CXX --version`"
 
 $CXX -std=c++11 *cpp -o notify || exit 1
 ./notify "input=test-a.1.txt" | diff - test-a.out.txt || exit 1
+./notify "input=test-b.1.txt" | diff - test-b.out.txt || exit 1
+./notify "input=test-z.1.txt" | diff - test-z.out.txt || exit 1
